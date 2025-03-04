f8d737475955bdc56555030a57c65599ee9d4702
shahinur001 <131193235+shahinur001@users.noreply.github.com>
2025-03-04 16:33:28 +0600

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-MONGODB_URI = "mongodb+srv://shahin:12267655@cluster0.gjsqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-MAX_FILE_SIZE = 10*1024*1024
-
-ALLOWED_TYPES = {"image/jpeg", "image/png", "application/pdf", "text/plain", "video/mp4", "audio/mp3"}
